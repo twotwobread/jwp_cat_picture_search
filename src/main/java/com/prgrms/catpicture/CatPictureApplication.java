469b30ed298cb5a4a0bddcee32b3f83fd77131c2
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-04 20:08:02 +0900

    [CHORE] 초기 개발 환경 세팅

---
+++ src/main/java/com/prgrms/catpicture/CatPictureApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.catpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatPictureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/catpicture/CatPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.catpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatPictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
