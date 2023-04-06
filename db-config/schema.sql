8efb2c78e0d4b9532693f8d7964c58ac68fbb35e
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-07 01:35:39 +0900

    [CHORE] 초기 docker DB 설정을 위한 sql 파일 추가.

--- db-config/schema.sql
+++ db-config/schema.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE IF NOT EXISTS `${MYSQL_DATABASE}`;
 USE `${MYSQL_DATABASE}`;
+
+CREATE TABLE IF NOT EXISTS cat_picture
+(
+    id          bigint  not null,
+    height      integer not null,
+    image_id    varchar(255) unique,
+    name        varchar(255),
+    origin      varchar(255),
+    temperament varchar(255),
+    url         varchar(255),
+    width       integer not null,
+    primary key (id)
+) engine = InnoDB;
