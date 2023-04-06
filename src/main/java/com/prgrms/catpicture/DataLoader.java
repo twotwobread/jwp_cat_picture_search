0788021a1a5d3a2aa4ff8ecbf785a601ae65d1a8
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-07 01:45:38 +0900

    [FEAT] CommandLineRunner를 구현하여 애플리케이션이 로딩될 때 데이터를 가져오게 구성 - 해당 기능이 실패해도 애플리케이션이 돌아갈 수 있게 Exception을 잡도록 구성.

---
+++ src/main/java/com/prgrms/catpicture/DataLoader.java
@@ -0,0 +1,27 @@
+package com.prgrms.catpicture;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.prgrms.catpicture.service.ExternalCatPictureService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+	private static final int MAX_LIMIT = 100;
+	private final ExternalCatPictureService externalCatPictureService;
+
+	@Override
+	public void run(String... args) {
+		try {
+			externalCatPictureService.loadCatPictures(MAX_LIMIT);
+		} catch (RuntimeException e) {
+			log.info("CommandLineRunner fails = ", e.getMessage());
+		}
+	}
+}
