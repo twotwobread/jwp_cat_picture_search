e3f39f70a3316adb119e29cdfe98e78f4b011cc7
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-07 15:04:07 +0900

    [TEST] 상수 값 변경

--- src/test/java/com/prgrms/catpicture/TestUtils.java
+++ src/test/java/com/prgrms/catpicture/TestUtils.java
@@ -9,7 +9,7 @@ public class TestUtils {
 	public static final String URL = "url";
 	public static final int WIDTH = 100;
 	public static final int HEIGHT = 100;
-	public static final String TEMPERAMENT = "url";
+	public static final String TEMPERAMENT = "temperament";
 	public static final String ORIGIN = "url";
 
 	public static CatPicture createCatPicture() {
