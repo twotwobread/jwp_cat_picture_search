fe6098d451726029b905955e4e0c7b1d883e3eab
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-07 15:15:18 +0900

    [FIX] 엔드 포인트에선 외부 API 호출하지 않게 수정.

--- src/main/java/com/prgrms/catpicture/controller/CatPictureController.java
+++ src/main/java/com/prgrms/catpicture/controller/CatPictureController.java
@@ -13,7 +13,6 @@ import com.prgrms.catpicture.common.ApiResponse;
 import com.prgrms.catpicture.dto.CatPictureDetailResponseDto;
 import com.prgrms.catpicture.dto.CatPictureResponseVo;
 import com.prgrms.catpicture.service.CatPictureService;
-import com.prgrms.catpicture.service.ExternalCatPictureService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,25 +21,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/cats")
 public class CatPictureController {
 
-	private static final int LIMIT = 50;
 	private final CatPictureService catPictureService;
-	private final ExternalCatPictureService externalCatPictureService;
 
 	@GetMapping("/random50")
 	public ResponseEntity<ApiResponse> getCatPicturesRandom50() {
-		try {
-			List<CatPictureResponseVo> response = externalCatPictureService.loadCatPictures(LIMIT).stream()
-				.map(CatPictureResponseVo::of)
-				.toList();
-			return ResponseEntity.ok(
-				ApiResponse.success(response)
-			);
-		} catch (RuntimeException e) {
-			List<CatPictureResponseVo> response = catPictureService.getCatPictures50();
-			return ResponseEntity.ok(
-				ApiResponse.success(response)
-			);
-		}
+		List<CatPictureResponseVo> response = catPictureService.getCatPictures50();
+		return ResponseEntity.ok(
+			ApiResponse.success(response)
+		);
 	}
 
 	@GetMapping("/search")
